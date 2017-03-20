a1a4392a3f7b3e3d5241a73f70f1ad5c778c7af2
Tobias Johansson <tobias@johansson.xyz>
2017-03-20 03:50:10 +0100

    Make code readable ckpt3

--- hops-tensorflow/src/main/java/io/hops/tensorflow/Client.java
+++ hops-tensorflow/src/main/java/io/hops/tensorflow/Client.java
@@ -460,7 +460,6 @@ public class Client {
   @VisibleForTesting
   @InterfaceAudience.Private
   public ApplicationId submitApplication() throws IOException, YarnException {
-
     LOG.info("Running Client");
     yarnClient.start();
 
@@ -480,137 +479,6 @@ public class Client {
     ContainerLaunchContext containerContext = createContainerLaunchContext(appResponse);
     ApplicationSubmissionContext appContext = createApplicationSubmissionContext(app, containerContext);
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    			
-
-    
-
-    
-
-    
-    
-
-    // Add AppMaster.jar location to classpath 		
-    // At some point we should not be required to add 
-    // the hadoop specific classpaths to the env. 
-    // It should be provided out of the box. 
-    // For now setting all required classpaths including
-    // the classpath to "." for the application jar
-    StringBuilder classPathEnv = new StringBuilder(Environment.CLASSPATH.$$())
-      .append(ApplicationConstants.CLASS_PATH_SEPARATOR).append("./*");
-    for (String c : conf.getStrings(
-        YarnConfiguration.YARN_APPLICATION_CLASSPATH,
-        YarnConfiguration.DEFAULT_YARN_CROSS_PLATFORM_APPLICATION_CLASSPATH)) {
-      classPathEnv.append(ApplicationConstants.CLASS_PATH_SEPARATOR);
-      classPathEnv.append(c.trim());
-    }
-    classPathEnv.append(ApplicationConstants.CLASS_PATH_SEPARATOR).append(
-      "./log4j.properties");
-
-    // add the runtime classpath needed for tests to work
-    if (conf.getBoolean(YarnConfiguration.IS_MINI_YARN_CLUSTER, false)) {
-      classPathEnv.append(':');
-      classPathEnv.append(System.getProperty("java.class.path"));
-    }
-
-    env.put("CLASSPATH", classPathEnv.toString());
-
-    // Set the necessary command to execute the application master 
-    Vector<CharSequence> vargs = new Vector<CharSequence>(30);
-
-    // Set java executable command 
-    LOG.info("Setting up app master command");
-    vargs.add(Environment.JAVA_HOME.$$() + "/bin/java");
-    // Set Xmx based on am memory size
-    vargs.add("-Xmx" + amMemory + "m");
-    // Set class name 
-    vargs.add(appMasterMainClass);
-    // Set params for Application Master
-    vargs.add("--container_memory " + String.valueOf(containerMemory));
-    vargs.add("--container_vcores " + String.valueOf(containerVirtualCores));
-    vargs.add("--num_containers " + String.valueOf(numContainers));
-    if (null != nodeLabelExpression) {
-      appContext.setNodeLabelExpression(nodeLabelExpression);
-    }
-    vargs.add("--priority " + String.valueOf(shellCmdPriority));
-
-    for (Map.Entry<String, String> entry : shellEnv.entrySet()) {
-      vargs.add("--shell_env " + entry.getKey() + "=" + entry.getValue());
-    }			
-    if (debugFlag) {
-      vargs.add("--debug");
-    }
-
-    vargs.add("1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/AppMaster.stdout");
-    vargs.add("2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/AppMaster.stderr");
-
-    // Get final commmand
-    StringBuilder command = new StringBuilder();
-    for (CharSequence str : vargs) {
-      command.append(str).append(" ");
-    }
-
-    LOG.info("Completed setting up app master command " + command.toString());	   
-    List<String> commands = new ArrayList<String>();
-    commands.add(command.toString());		
-
-    // Set up the container launch context for the application master
-    ContainerLaunchContext amContainer = ContainerLaunchContext.newInstance(
-      localResources, env, commands, null, null, null);
-
-    // Set up resource type requirements
-    // For now, both memory and vcores are supported, so we set memory and 
-    // vcores requirements
-    Resource capability = Resource.newInstance(amMemory, amVCores);
-    appContext.setResource(capability);
-
-    // Service data is a binary blob that can be passed to the application
-    // Not needed in this scenario
-    // amContainer.setServiceData(serviceData);
-
-    // Setup security tokens
-    if (UserGroupInformation.isSecurityEnabled()) {
-      // Note: Credentials class is marked as LimitedPrivate for HDFS and MapReduce
-      Credentials credentials = new Credentials();
-      String tokenRenewer = conf.get(YarnConfiguration.RM_PRINCIPAL);
-      if (tokenRenewer == null || tokenRenewer.length() == 0) {
-        throw new IOException(
-          "Can't get Master Kerberos principal for the RM to use as renewer");
-      }
-
-      // For now, only getting tokens for the default file-system.
-      final Token<?> tokens[] =
-          fs.addDelegationTokens(tokenRenewer, credentials);
-      if (tokens != null) {
-        for (Token<?> token : tokens) {
-          LOG.info("Got dt for " + fs.getUri() + "; " + token);
-        }
-      }
-      DataOutputBuffer dob = new DataOutputBuffer();
-      credentials.writeTokenStorageToStream(dob);
-      ByteBuffer fsTokens = ByteBuffer.wrap(dob.getData(), 0, dob.getLength());
-      amContainer.setTokens(fsTokens);
-    }
-
-    appContext.setAMContainerSpec(amContainer);
-
-    // Set the priority for the application master
-    // TODO - what is the range for priority? how to decide? 
-    Priority pri = Priority.newInstance(amPriority);
-    appContext.setPriority(pri);
-
-    // Set the queue to which this application is to be submitted in the RM
-    appContext.setQueue(amQueue);
-
     // Submit the application to the applications manager
     // SubmitApplicationResponse submitResp = applicationsManager.submitApplication(appRequest);
     // Ignore the response as either a valid response object is returned on success 
@@ -689,15 +557,124 @@ public class Client {
     }
   }
   
-  private ContainerLaunchContext createContainerLaunchContext(GetNewApplicationResponse appResponse) {
+  private ContainerLaunchContext createContainerLaunchContext(GetNewApplicationResponse appResponse)
+      throws IOException {
+    FileSystem fs = FileSystem.get(conf);
+    ApplicationId appId = appResponse.getApplicationId();
+  
+    Map<String, String> launchEnv = setupLaunchEnv(fs, appId);
+    Map<String, LocalResource> localResources = prepareLocalResources(fs, appId);
+  
+    // Set the necessary command to execute the application master
+    Vector<CharSequence> vargs = new Vector<CharSequence>(30);
+  
+    // Set java executable command
+    LOG.info("Setting up app master command");
+    vargs.add(Environment.JAVA_HOME.$$() + "/bin/java");
+    // Set Xmx based on am memory size
+    vargs.add("-Xmx" + amMemory + "m");
+    // Set class name
+    vargs.add(appMasterMainClass);
+    // Set params for Application Master
+    vargs.add("--container_memory " + String.valueOf(containerMemory));
+    vargs.add("--container_vcores " + String.valueOf(containerVirtualCores));
+    vargs.add("--num_containers " + String.valueOf(numContainers));
+    vargs.add("--priority " + String.valueOf(shellCmdPriority));
+  
+    for (Map.Entry<String, String> entry : shellEnv.entrySet()) {
+      vargs.add("--shell_env " + entry.getKey() + "=" + entry.getValue());
+    }
+    if (debugFlag) {
+      vargs.add("--debug");
+    }
+  
+    vargs.add("1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/AppMaster.stdout");
+    vargs.add("2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/AppMaster.stderr");
+  
+    // Get final commmand
+    StringBuilder command = new StringBuilder();
+    for (CharSequence str : vargs) {
+      command.append(str).append(" ");
+    }
+  
+    LOG.info("Completed setting up app master command " + command.toString());
+    List<String> commands = new ArrayList<String>();
+    commands.add(command.toString());
+  
+    // Set up the container launch context for the application master
+    ContainerLaunchContext amContainer = ContainerLaunchContext.newInstance(
+        localResources, launchEnv, commands, null, null, null);
+  
     // Set the necessary security tokens as needed
-    //amContainer.setContainerTokens(containerToken);
+    // amContainer.setContainerTokens(containerToken);
+    
+    // Service data is a binary blob that can be passed to the application
+    // Not needed in this scenario
+    // amContainer.setServiceData(serviceData);
   
-  
-    return null;
+    // Setup security tokens
+    if (UserGroupInformation.isSecurityEnabled()) {
+      // Note: Credentials class is marked as LimitedPrivate for HDFS and MapReduce
+      Credentials credentials = new Credentials();
+      String tokenRenewer = conf.get(YarnConfiguration.RM_PRINCIPAL);
+      if (tokenRenewer == null || tokenRenewer.length() == 0) {
+        throw new IOException(
+            "Can't get Master Kerberos principal for the RM to use as renewer");
+      }
+    
+      // For now, only getting tokens for the default file-system.
+      final Token<?> tokens[] =
+          fs.addDelegationTokens(tokenRenewer, credentials);
+      if (tokens != null) {
+        for (Token<?> token : tokens) {
+          LOG.info("Got dt for " + fs.getUri() + "; " + token);
+        }
+      }
+      DataOutputBuffer dob = new DataOutputBuffer();
+      credentials.writeTokenStorageToStream(dob);
+      ByteBuffer fsTokens = ByteBuffer.wrap(dob.getData(), 0, dob.getLength());
+      amContainer.setTokens(fsTokens);
+    }
+    
+    return amContainer;
   }
   
-  private Map<String, String> setupLaunchEnv() {
+  private ApplicationSubmissionContext createApplicationSubmissionContext(YarnClientApplication app,
+      ContainerLaunchContext containerContext) {
+    // set the application name
+    ApplicationSubmissionContext appContext = app.getApplicationSubmissionContext();
+  
+    appContext.setKeepContainersAcrossApplicationAttempts(keepContainers);
+    appContext.setApplicationName(appName);
+  
+    if (attemptFailuresValidityInterval >= 0) {
+      appContext.setAttemptFailuresValidityInterval(attemptFailuresValidityInterval);
+    }
+  
+    if (null != nodeLabelExpression) {
+      appContext.setNodeLabelExpression(nodeLabelExpression);
+    }
+  
+    // Set up resource type requirements
+    // For now, both memory and vcores are supported, so we set memory and
+    // vcores requirements
+    Resource capability = Resource.newInstance(amMemory, amVCores);
+    appContext.setResource(capability);
+  
+    appContext.setAMContainerSpec(containerContext);
+  
+    // Set the priority for the application master
+    // TODO - what is the range for priority? how to decide?
+    Priority pri = Priority.newInstance(amPriority);
+    appContext.setPriority(pri);
+  
+    // Set the queue to which this application is to be submitted in the RM
+    appContext.setQueue(amQueue);
+    
+    return appContext;
+  }
+  
+  private Map<String, String> setupLaunchEnv(FileSystem fs, ApplicationId appId) throws IOException {
     // The shell script has to be made available on the final container(s)
     // where it will be executed.
     // To do this, we need to first copy into the filesystem that is visible
@@ -719,11 +696,11 @@ public class Client {
       hdfsShellScriptLen = shellFileStatus.getLen();
       hdfsShellScriptTimestamp = shellFileStatus.getModificationTime();
     }
-  
+    
     // Set the env variables to be setup in the env where the application master will be run
     LOG.info("Set the environment for the application master");
     Map<String, String> env = new HashMap<String, String>();
-  
+    
     // put location of shell script into env
     // using the env info, the application master will create the correct local resource for the
     // eventual containers that will be launched to execute the shell scripts
@@ -734,33 +711,57 @@ public class Client {
       env.put(DSConstants.DISTRIBUTEDSHELLTIMELINEDOMAIN, domainId);
     }
     
-    return null;
+    // Add AppMaster.jar location to classpath
+    // At some point we should not be required to add
+    // the hadoop specific classpaths to the env.
+    // It should be provided out of the box.
+    // For now setting all required classpaths including
+    // the classpath to "." for the application jar
+    StringBuilder classPathEnv = new StringBuilder(Environment.CLASSPATH.$$())
+        .append(ApplicationConstants.CLASS_PATH_SEPARATOR).append("./*");
+    for (String c : conf.getStrings(
+        YarnConfiguration.YARN_APPLICATION_CLASSPATH,
+        YarnConfiguration.DEFAULT_YARN_CROSS_PLATFORM_APPLICATION_CLASSPATH)) {
+      classPathEnv.append(ApplicationConstants.CLASS_PATH_SEPARATOR);
+      classPathEnv.append(c.trim());
+    }
+    classPathEnv.append(ApplicationConstants.CLASS_PATH_SEPARATOR).append(
+        "./log4j.properties");
+    
+    // add the runtime classpath needed for tests to work
+    if (conf.getBoolean(YarnConfiguration.IS_MINI_YARN_CLUSTER, false)) {
+      classPathEnv.append(':');
+      classPathEnv.append(System.getProperty("java.class.path"));
+    }
+    
+    env.put("CLASSPATH", classPathEnv.toString());
+    
+    return env;
   }
   
-  private Map<String, LocalResource> prepareLocalResources() {
+  private Map<String, LocalResource> prepareLocalResources(FileSystem fs, ApplicationId appId) throws IOException {
     // set local resources for the application master
     // local files or archives as needed
     // In this scenario, the jar file for the application master is part of the local resources
     Map<String, LocalResource> localResources = new HashMap<String, LocalResource>();
-  
+    
     LOG.info("Copy App Master jar from local filesystem and add to local environment");
     // Copy the application master jar to the filesystem
     // Create a local resource to point to the destination jar path
-    FileSystem fs = FileSystem.get(conf);
     addToLocalResources(fs, appMasterJar, appMasterJarPath, appId.toString(),
         localResources, null);
-  
+    
     // Set the log4j properties if needed
     if (!log4jPropFile.isEmpty()) {
       addToLocalResources(fs, log4jPropFile, log4jPath, appId.toString(),
           localResources, null);
     }
-  
+    
     if (!shellCommand.isEmpty()) {
       addToLocalResources(fs, null, shellCommandPath, appId.toString(),
           localResources, shellCommand);
     }
-  
+    
     if (shellArgs.length > 0) {
       addToLocalResources(fs, null, shellArgsPath, appId.toString(),
           localResources, StringUtils.join(shellArgs, " "));
@@ -769,23 +770,6 @@ public class Client {
     return localResources;
   }
   
-  private ApplicationSubmissionContext createApplicationSubmissionContext(YarnClientApplication app,
-      ContainerLaunchContext containerContext) {
-    // set the application name
-    ApplicationSubmissionContext appContext = app.getApplicationSubmissionContext();
-    ApplicationId appId = appContext.getApplicationId();
-  
-    appContext.setKeepContainersAcrossApplicationAttempts(keepContainers);
-    appContext.setApplicationName(appName);
-  
-    if (attemptFailuresValidityInterval >= 0) {
-      appContext
-          .setAttemptFailuresValidityInterval(attemptFailuresValidityInterval);
-    }
-    
-    return appContext;
-  }
-
   /**
    * Monitor the submitted application for completion. 
    * Kill application if time expires. 
