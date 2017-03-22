778992b6df8b12ae666f85aac1639b9970843c19
Tobias Johansson <tobias@johansson.xyz>
2017-03-22 04:39:18 +0100

    Silly renaming

--- hops-tensorflow/src/main/java/io/hops/tensorflow/Constants.java
+++ hops-tensorflow/src/main/java/io/hops/tensorflow/Constants.java
@@ -21,7 +21,7 @@ public class Constants {
   
   public static final String AM_JAR_PATH = "AppMaster.jar";
   public static final String ARGS_PATH = "appArgs";
-  public static final String DIST_CACHE_PATH = "distCacheList.ser";
+  public static final String DIST_CACHE_PATH = "distCache.ser";
   public static final String LOCALIZED_PYTHON_DIR = "__pyfiles__";
   public static final String LOG4J_PATH = "log4j.properties";
   public static final String YARNTF_STAGING = ".yarntfStaging";
